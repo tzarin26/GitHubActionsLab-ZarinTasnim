ede7d8328e3e324adb7a5b0569a0512d0ba0ce57
zarin <tasnimz@sheridancollege.ca>
2026-06-15 08:57:10 -0400

    new project

---
+++ src/ice5_zarintasnim/Ice5_ZarinTasnim.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package ice5_zarintasnim;
+
+/**
+ *
+ * @author Zarin Tasnim, 2026
+ */
+public class Ice5_ZarinTasnim {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
